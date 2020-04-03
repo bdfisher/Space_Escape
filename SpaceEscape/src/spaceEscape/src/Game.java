55cd8f3d7fa46e0c661d3f64f5006b92eea7b199
Mike <mdbehnke@iastate.edu>
2020-04-03 09:15:50 -0500

    Stopped using windowbuilder

---
+++ SpaceEscape/src/spaceEscape/src/Game.java
@@ -0,0 +1,56 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Font;
+
+public class Game {
+	
+	JFrame window;
+	Container con;
+	JPanel titleNamePanel, startButtonPanel;
+	JLabel titleNameLabel;
+	Font titleFont = new Font("Times New Roman", Font.PLAIN, 84);
+	JButton startButton;
+	
+	public static void main(String [] args)
+	{
+		new Game();
+	}
+	
+	public Game()
+	{
+		window = new JFrame();
+		window.setSize(800, 600);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.getContentPane().setBackground(Color.black);
+		window.setLayout(null);
+		con = window.getContentPane();
+		
+		titleNamePanel = new JPanel();
+		titleNamePanel.setBounds(100, 100, 600, 150);
+		titleNamePanel.setBackground(Color.black);
+		titleNameLabel = new JLabel("SPACE ESCAPE");
+		titleNameLabel.setForeground(Color.white);
+		titleNameLabel.setFont(titleFont);
+		
+		startButtonPanel = new JPanel();
+		startButtonPanel.setBounds(300, 400, 200, 100);
+		startButtonPanel.setBackground(Color.black);
+		
+		startButton = new JButton("START");
+		startButton.setBackground(Color.white);
+		startButton.setForeground(Color.black);
+		
+		titleNamePanel.add(titleNameLabel);
+		startButtonPanel.add(startButton);
+		
+		con.add(titleNamePanel);
+		con.add(startButtonPanel);
+		
+		window.setVisible(true);
+	}
+}
