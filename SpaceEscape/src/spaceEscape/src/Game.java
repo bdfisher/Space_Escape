d9bb5a22f1109e5f199d60e3fb90a5fd07583c6a
Mike <mdbehnke@iastate.edu>
2020-04-03 10:06:24 -0500

    Got the start button working, beginning to work on other buttons

--- SpaceEscape/src/spaceEscape/src/Game.java
+++ SpaceEscape/src/spaceEscape/src/Game.java
@@ -2,19 +2,27 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Game {
 	
 	JFrame window;
 	Container con;
-	JPanel titleNamePanel, startButtonPanel;
+	JPanel titleNamePanel, startButtonPanel, mainTextPanel, choiceButtonPanel;
 	JLabel titleNameLabel;
 	Font titleFont = new Font("Times New Roman", Font.PLAIN, 84);
-	JButton startButton;
+	Font normalFont = new Font("Times New Roman", Font.PLAIN, 30);
+	JButton startButton, choice1, choice2, choice3, choice4;
+	JTextArea mainTextArea;
+	
+	TitleScreenHandler tsHandler = new TitleScreenHandler();
 	
 	public static void main(String [] args)
 	{
@@ -44,6 +52,8 @@ public class Game {
 		startButton = new JButton("START");
 		startButton.setBackground(Color.white);
 		startButton.setForeground(Color.black);
+		startButton.setFont(normalFont);
+		startButton.addActionListener(tsHandler);
 		
 		titleNamePanel.add(titleNameLabel);
 		startButtonPanel.add(startButton);
@@ -52,5 +62,64 @@ public class Game {
 		con.add(startButtonPanel);
 		
 		window.setVisible(true);
+	}
+	
+	public void createGameScreen()
+	{
+		titleNamePanel.setVisible(false);
+		startButtonPanel.setVisible(false);
+		
+		mainTextPanel = new JPanel();
+		mainTextPanel.setBounds(100, 100, 600, 250);
+		mainTextPanel.setBackground(Color.black);
+		con.add(mainTextPanel);
+		
+		mainTextArea = new JTextArea("You wake up, head pounding, in a dark small cell in an unknown place. You are hungry and afraid. You can hear weird voices, in a language you've never heard before. Do you investigate or not?");
+		mainTextArea.setBounds(100, 100, 600, 250);
+		mainTextArea.setBackground(Color.black);
+		mainTextArea.setForeground(Color.white);
+		mainTextArea.setFont(normalFont);
+		mainTextArea.setLineWrap(true);
+		mainTextPanel.add(mainTextArea);
+		
+		choiceButtonPanel = new JPanel();
+		choiceButtonPanel.setBounds(250, 350, 300, 150);
+		choiceButtonPanel.setBackground(Color.black);
+		choiceButtonPanel.setLayout(new GridLayout(4, 1));
+		con.add(choiceButtonPanel);
+		
+		choice1 = new JButton("Investigate");
+		choice1.setBackground(Color.white);
+		choice1.setForeground(Color.black);
+		choice1.setFont(normalFont);
+		choiceButtonPanel.add(choice1);
+		choice2 = new JButton("Don't Investigate");
+		choice2.setBackground(Color.white);
+		choice2.setForeground(Color.black);
+		choice2.setFont(normalFont);
+		choiceButtonPanel.add(choice2);
+		
+		/*
+		choice3 = new JButton();
+		choice3.setBackground(Color.white);
+		choice3.setForeground(Color.black);
+		choice3.setFont(normalFont);
+		choiceButtonPanel.add(choice3);
+		choice4 = new JButton();
+		choice4.setBackground(Color.white);
+		choice4.setForeground(Color.black);
+		choice4.setFont(normalFont);
+		choiceButtonPanel.add(choice4);
+		*/
+		
+		
+	}
+	
+	public class TitleScreenHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent event)
+		{
+			createGameScreen();
+		}
 	}
 }
